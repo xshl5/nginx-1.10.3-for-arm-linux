ea92d37951aed19c5adcb72ec37b0623f3c8b65d
xshl5 <xshl5@163.com>
2022-03-21 19:21:59 +0800

    Initial import.

---
+++ nginx-rtmp-module/ngx_rtmp_version.h
@@ -0,0 +1,15 @@
+
+/*
+ * Copyright (C) Roman Arutyunyan
+ */
+
+
+#ifndef _NGX_RTMP_VERSION_H_INCLUDED_
+#define _NGX_RTMP_VERSION_H_INCLUDED_
+
+
+#define nginx_rtmp_version  1002002
+#define NGINX_RTMP_VERSION  "1.2.2-dev"
+
+
+#endif /* _NGX_RTMP_VERSION_H_INCLUDED_ */
